1c9004e3a04c1caaaad5f26267eb8997943f4c68
Arina Mykhova <arinabagira@mail.ru>
2020-12-10 21:40:18 +0300

    сделать добавление новой карточкм + лайк

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,49 @@
-// let formElement = document.querySelector('.popup');
 let nameInput = document.querySelector('.popup__name');
 let jobInput = document.querySelector('.popup__job');
 let profileName = document.querySelector('.profile__name');;
 let profileJob = document.querySelector('.profile__job');
 let editForm = document.querySelector('.profile__button-edit');
-let closeProfile = document.querySelector('#close-profile');
-let closeGallery = document.querySelector('#close-gallery');
-let form = document.querySelector('#popup-profile');
-const openFormAddCard = document.querySelector('#popup-gallery');
-const profileForm = document.querySelector('#profile-form');
-const galleryForm = document.querySelector('#gallery-form');
+let closeProfile = document.querySelector('.popup__close_proile');
+let closeGallery = document.querySelector('.popup__close_gallery');
+let form = document.querySelector('.popup__profile');
+const openFormAddCard = document.querySelector('.popup__gallery');
+const profileForm = document.querySelector('.profile__form');
+const galleryForm = document.querySelector('.gallery__form');
 const cardContainerElement = document.querySelector('.gallery');
 const buttonAddCard = document.querySelector('.profile__button-add');
+const inputNameGallery = document.querySelector('.popup__name_gallery');
+const inputLinkGallery = document.querySelector('.popup__link');
+const templateElement = document.querySelector('.template');
+let likeButton;
 const initialCards = [{
         name: 'Камчатка',
-        img: './images/kamchatka.jpg'
+        img: './images/kamchatka.jpg',
+        alt: 'Камчатка'
     },
     {
         name: 'Байкал',
-        img: './images/baikal.jpg'
+        img: './images/baikal.jpg',
+        alt: 'Байкал'
     },
     {
         name: 'Хужир',
-        img: './images/Khuzhir.jpg'
+        img: './images/Khuzhir.jpg',
+        alt: 'Хужир'
     },
     {
         name: 'Иваново',
-        img: './images/ivanovo.jpg'
+        img: './images/ivanovo.jpg',
+        alt: 'Иваново'
     },
     {
         name: 'Эльбрус',
-        img: './images/elbrus.jpg'
+        img: './images/elbrus.jpg',
+        alt: 'Эльбрус'
     },
     {
         name: 'Тулиновка',
-        img: './images/tulinovka.jpg'
+        img: './images/tulinovka.jpg',
+        alt: 'Тулиновка'
     }
 ];
 
@@ -43,37 +52,36 @@ formSubmitHandler = (evt) => {
     evt.preventDefault();
     profileName.textContent = nameInput.value === '' ? profileName.textContent : nameInput.value;
     profileJob.textContent = jobInput.value === '' ? profileJob.textContent : jobInput.value;
-    hideClick();
+    hideClick(evt);
 }
 form.addEventListener('submit', formSubmitHandler);
 
 showClick = (evt) => {
-    switch (evt.currentTarget.typeButton) {
-        case 'button-profile':
-            nameInput.value = profileName.textContent;
-            jobInput.value = profileJob.textContent;
-            form.classList.add('popup_opened');
-            break;
-        case 'button-card':
-            openFormAddCard.classList.add('popup_opened');
-            break;
+    if (evt.target.classList.contains('profile__button-edit')) {
+        nameInput.value = profileName.textContent;
+        jobInput.value = profileJob.textContent;
+        form.classList.add('popup_opened');
+        return;
+    }
+    if (evt.target.classList.contains('profile__button-add')) {
+        openFormAddCard.classList.add('popup_opened');
+        return;
     }
 
 }
 
 editForm.addEventListener('click', showClick);
-editForm.typeButton = 'button-profile';
+
 
 hideClick = (evt) => {
-    switch (evt.currentTarget.typeButton) {
-        case 'button-profile':
-            profileForm.reset();
-            form.classList.remove('popup_opened');
-            break;
-        case 'button-card':
-            galleryForm.reset();
-            openFormAddCard.classList.remove('popup_opened');
-            break;
+    if (evt.target.classList.contains('popup__close_proile') || evt.target.classList.contains('profile__form')) {
+        profileForm.reset();
+        form.classList.remove('popup_opened');
+        return;
+    }
+    if (evt.target.classList.contains('popup__close_gallery') || evt.target.classList.contains('gallery__form')) {
+        openFormAddCard.classList.remove('popup_opened');
+        return;
     }
 
 }
@@ -84,30 +92,45 @@ closeGallery.addEventListener('click', hideClick);
 closeGallery.typeButton = 'button-card';
 
 buttonAddCard.addEventListener('click', showClick);
-buttonAddCard.typeButton = 'button-card';
+
 
 function renderGallery() {
-    let newHTML = '';
-    newHTML = initialCards.map(composeCardHTML).join('');
-    cardContainerElement.insertAdjacentHTML('afterbegin', newHTML);
+    const galleryCards = initialCards.map(composeCard);
+    cardContainerElement.append(...galleryCards);
 }
 
-function composeCardHTML(item) {
-    return `<article class="gallery__card">
-    <img class="gallery__image"src='${item.img}'alt="Камчатка">
-    <div class="gallery__wrapper">
-        <h2 class="gallery__name">${item.name}</h2>
-        <button class="gallery__button"ntype="button" name="button-like"></button>
-    </div>
-</article>`
+function composeCard(item) {
+    const newCard = templateElement.content.cloneNode(true);
+    const headerElement = newCard.querySelector('.gallery__name');
+    const imageElement = newCard.querySelector('.gallery__image');
+    const imageAlt = newCard.querySelector('.gallery__image');
+    headerElement.textContent = item.name;
+    imageElement.src = item.img;
+    imageAlt.alt = item.alt;
+    return newCard;
 }
 
-// function bindAddCardListener() {
-//     const addButtonElement = document.querySelector('.profile__button-add');
-//     addButtonElement.addEventListener('click', () => {
+galleryForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    addNewCard();
+    hideClick(evt);
+});
 
-//     })
-// }
+function addNewCard() {
+    const inputText = inputNameGallery.value;
+    const inputLink = inputLinkGallery.value;
+
+    const newCardHTML = composeCard({ name: inputText, img: inputLink, alt: 'Здесь ваша картинка' });
+    cardContainerElement.prepend(newCardHTML);
+}
+
+function likeButtonActive() {
+    likeButton = document.querySelectorAll('.gallery__button');
+    likeButton.forEach(function(item) {
+        item.addEventListener('click', () => item.classList.toggle('gallery__button_like'));
+    });
+
+}
 
 renderGallery();
-// bindAddCardListener();
+likeButtonActive();
