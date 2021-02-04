6c784ebfa765e36a7876175938a6c1f38fdc2be5
Arina Mykhova <arinabagira@mail.ru>
2021-02-05 00:15:00 +0300

    сделать новую тему

--- scripts/initial-cards.js
+++ scripts/initial-cards.js
@@ -1,31 +1,31 @@
 export const initialCards = [{
-        name: 'Камчатка',
-        img: './images/kamchatka.jpg',
-        alt: 'Камчатка'
+        name: 'The Lion King',
+        img: 'https://i.pinimg.com/originals/f0/39/47/f03947cac66d0f9b3f9b4af7ef916763.jpg',
+        alt: 'The Lion King'
     },
     {
-        name: 'Байкал',
-        img: './images/baikal.jpg',
-        alt: 'Байкал'
+        name: 'Bambi',
+        img: 'https://www.animationsource.org/sites_content/bambi/img_screenshot/114363.jpg',
+        alt: 'Bambi'
     },
     {
-        name: 'Хужир',
-        img: './images/Khuzhir.jpg',
-        alt: 'Хужир'
+        name: 'Chip n Dale',
+        img: 'https://pbs.twimg.com/media/C70Ewv6W4AABV8Q.jpg',
+        alt: 'Chip n Dale'
     },
     {
-        name: 'Иваново',
-        img: './images/ivanovo.jpg',
-        alt: 'Иваново'
+        name: 'Dumbo',
+        img: 'https://avatars.mds.yandex.net/get-zen_doc/61795/pub_5c92526f19fa6800b3c9ae5c_5c9255c8d8c70400b330e221/scale_1200',
+        alt: 'Dumbo'
     },
     {
-        name: 'Эльбрус',
-        img: './images/elbrus.jpg',
-        alt: 'Эльбрус'
+        name: 'Robin Hood',
+        img: 'https://www.brain-games.ru/upload/iblock/d78/0_b1688_b6acbc1c_orig.png',
+        alt: 'Robin Hood'
     },
     {
-        name: 'Тулиновка',
-        img: './images/tulinovka.jpg',
-        alt: 'Тулиновка'
+        name: 'Lilo & Stitch',
+        img: 'https://disney-planet.fr/wp-content/uploads/2016/07/stitch-personnage-lilo-stitch-07.jpg',
+        alt: 'Lilo & Stitch'
     }
 ];
